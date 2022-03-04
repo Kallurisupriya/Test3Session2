6500c0f797d24fd31c94381ab8fa28306c662def
Kalluri Supriya <94670471+Kallurisupriya@users.noreply.github.com>
2022-03-04 09:50:24 +0000

    excited

--- p1final.c
+++ p1final.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+void input(int *num1,int *den1, int *num2,int *den2)
+{
+  printf("enter the numerators of two fractions\n");
+  scanf("%d%d",num1,num2);
+  printf("enter the denominators of two fractions\n");
+  scanf("%d%d",den1,den2);
+}
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+  int gcd,lcm,i;
+  *num3=(num1*den2)+(num2*den1);
+  *den3=(den1*den2);
+  for(i=1;i<=den1&&i<=den2;++i)
+    {
+      if(den1%i==0&&den2%i==0)
+        gcd=i;
+    }
+  *num3=*num3/gcd;
+  *den3=*den3/gcd;
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{ printf("%d/%d+%d/%d=%d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,num2,num3,den1,den2,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,30 @@
 #include<stdio.h>
+struct_fraction
+{
+  int num,den;
+};
+typedef _fraction Fraction
+Fraction input_fraction()
+{
+  Fraction val1;
+  scanf("%d%d",&val1.num,&val1.den);
+  return val1;
+}
+Fraction Largest_fraction(Fraction f1,Fraction f2,fraction f3)
+{
+  Fraction result;
+  int i,lcm,fr1,fr2,fr3;
+  lcm=f1.den*f2.den*f3.den;
+  fr1=f1.num*lcm;
+  fr2=f2.num*lcm;
+  fr3=f3.num*lcm;
+  for(i=1;i<=f1.den&&i<=f2.den&&i<=f3.den;++i)
+  {
+    if(f1.den%i==0&&f2.den%i==0&&f3.den%i==0)
+      gcd=i;
+  }
+}
+if(fr1<fr2&&fr2<fr3)
+{
+  result=
+}
