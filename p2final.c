417e8223c7efd47345a4b897399e62ba05f9d201
Kalluri Supriya <94670471+Kallurisupriya@users.noreply.github.com>
2022-03-04 10:31:33 +0000

    excited

--- p2final.c
+++ p2final.c
@@ -1,30 +1,42 @@
 #include<stdio.h>
-struct_fraction
+typedef struct fraction
 {
-  int num,den;
-};
-typedef _fraction Fraction
+int num,den;
+}Fraction;
 Fraction input_fraction()
 {
   Fraction val1;
   scanf("%d%d",&val1.num,&val1.den);
   return val1;
 }
-Fraction Largest_fraction(Fraction f1,Fraction f2,fraction f3)
+Fraction smallest_fraction(Fraction f1, Fraction f2, Fraction f3) 
 {
-  Fraction result;
-  int i,lcm,fr1,fr2,fr3;
+  int lcm,fr1,fr2,fr3;
   lcm=f1.den*f2.den*f3.den;
-  fr1=f1.num*lcm;
-  fr2=f2.num*lcm;
-  fr3=f3.num*lcm;
-  for(i=1;i<=f1.den&&i<=f2.den&&i<=f3.den;++i)
-  {
-    if(f1.den%i==0&&f2.den%i==0&&f3.den%i==0)
-      gcd=i;
-  }
+  fr1=f1.num*lcm/f1.den;
+  fr2=f2.num*lcm/f2.den;
+  fr3=f3.num*lcm/f3.den;
+  if(fr1<fr2&&fr1<fr3)
+    return f1;
+  else
+    if(fr2<fr3&&fr2<fr1)
+      return f2;
+  else
+      if(fr3<fr1&&fr3<fr2)
+        return f3;
 }
-if(fr1<fr2&&fr2<fr3)
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction smallest)
 {
-  result=
+  printf("the smallest of %d/%d,%d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
+}
+int main()
+{
+  Fraction f1,f2,f3,result;
+  printf("enter 3 fractions\n");
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  result=smallest_fraction(f1,f2,f3);
+  output(f1,f2,f3,result);
+  return 0;
 }
